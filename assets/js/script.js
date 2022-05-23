2072cf525a6a0a412b7060742428ac4819df421e
Ommar Khawaja <ommarkhawaja1@gmail.com>
2022-05-23 14:44:40 -0400

    display question and timer on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,28 @@
-var startButton = document.querySelector(".start_btn");
-var timerElement = document.querySelector(".timer_count");
-var question = document.querySelector(".question");
+var startButton = document.querySelector(".start-btn");
+var questionContainerElement = document.querySelector("#question-container");
+var timerElement = document.querySelector(".timer-count");
+var questionElement = document.querySelector("#question");
+var answerButtonsElement = document.querySelector(".answer-buttons")
+let shuffledQuestions, currentQuestionIndex
 var timer;
 
-
-var questions = [
-    { question: "How do we enclose an HTML tag",
-    choices: ["{}", "()", "<>", "[]"],
-    correctAnswer: "<>"},
-
-    { question: "How do we enclose an HTML tag",
-    choices: ["{}", "()", "<>", "[]"],
-    correctAnswer: "<>"},
-    
-]
 
 //  attaching an event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
 
+
 function startGame() {
+    console.log("started");
+    startButton.classList.add("hide");
+    questionContainerElement.classList.remove("hide")
+    shuffledQuestions = questions.sort(() => Math.random() -.5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove("hide")
+    setNextQuestion()
+
     timerCount = 40;
-    startTimer()
+    startTimer();
+
 };
 
 
@@ -35,5 +37,32 @@ function startTimer() {
         }
     }, 1000);
 
-    }
+};
 
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: "How do we enclose an HTML tag",
+        choices: ["{}", "()", "<>", "[]"],
+        correctAnswer: "<>"
+    },
+
+    {
+        question: "How do we enclose an HTML tag",
+        choices: ["{}", "()", "<>", "[]"],
+        correctAnswer: "<>"
+    },
+
+]
